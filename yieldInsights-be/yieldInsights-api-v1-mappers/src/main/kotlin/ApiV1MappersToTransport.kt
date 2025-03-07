314664c66e825208656f0d6af98401a8668586e7
Koferz <koferz104@gmail.com>
2025-03-07 12:49:31 +0400

    Merge main into develop
    * hw-3 third homeWork
    
    * [hw-4]  fourth homeWork
    
    * hw-4 add specs
    
    * hw-4 add specs
    
    * hw-4 add api mappers
    
    * hw-4 add mapper test
    
    ---------
    
    Co-authored-by: Kozyrev Stanislav <kozyrev.stanislav@rt.ru>
    
    ---------
    
    Co-authored-by: Kozyrev Stanislav <kozyrev.stanislav@rt.ru>

---
+++ yieldInsights-be/yieldInsights-api-v1-mappers/src/main/kotlin/ApiV1MappersToTransport.kt
@@ -0,0 +1,87 @@
+import exceptions.UnknownCommand
+import models.*
+import ru.otus.yieldInsights.api.v1.models.*
+
+
+fun  Context.toTransport(): IResponse = when (val cmd = command) {
+    Command.CREATE -> toTransportCreate()
+    Command.READ -> toTransportRead()
+    Command.UPDATE -> toTransportUpdate()
+    Command.DELETE -> toTransportDelete()
+    Command.SEARCH -> toTransportSearch()
+    Command.CLOSING -> toTransportClosing()
+    Command.NONE -> throw UnknownCommand(cmd)
+}
+
+fun Context.toTransportCreate() = DepositCreateResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposit = depositResponse.toTransport()
+)
+
+fun Context.toTransportRead() = DepositReadResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposit = depositResponse.toTransport()
+)
+
+fun Context.toTransportUpdate() = DepositUpdateResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposit = depositResponse.toTransport()
+)
+
+fun Context.toTransportDelete() = DepositDeleteResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposit = depositResponse.toTransport()
+)
+
+fun Context.toTransportSearch() = DepositSearchResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposits = depositsResponse.toTransport()
+)
+
+fun Context.toTransportClosing() = DepositClosingResponse(
+    result = state.toResult(),
+    errors = errors.toTransportErrors(),
+    deposits = depositsResponse.toTransport()
+)
+
+fun List<Deposit>.toTransport(): List<DepositResponseObject>? = this
+    .map { it.toTransport() }
+    .toList()
+    .takeIf { it.isNotEmpty() }
+
+private fun Deposit.toTransport(): DepositResponseObject = DepositResponseObject(
+    id = depositId.takeIf { it != DepositId.NONE }?.asString(),
+    bankName = bankName.toString(),
+    depositName = depositName.takeIf { it.isNotBlank() },
+    rate = rate.takeIf { it.isNotBlank() },
+    openingDate = openingDate.takeIf { it.isNotBlank() },
+    depositTerm = depositTerm.takeIf { it.isNotBlank() },
+    depositAmount = depositAmount.takeIf { it.isNotBlank() },
+    depositOperation = depositOperation.toString(),
+    isActive = isActive.toString(),
+    ownerId = ownerId.takeIf { it != UserId.NONE }?.asString(),
+)
+
+private fun List<DepositError>.toTransportErrors(): List<Error>? = this
+    .map { it.toTransport() }
+    .toList()
+    .takeIf { it.isNotEmpty() }
+
+private fun DepositError.toTransport() = Error(
+    code = code.takeIf { it.isNotBlank() },
+    group = group.takeIf { it.isNotBlank() },
+    field = field.takeIf { it.isNotBlank() },
+    message = message.takeIf { it.isNotBlank() },
+)
+
+private fun State.toResult(): ResponseResult? = when (this) {
+    State.RUNNING -> ResponseResult.SUCCESS
+    State.FAILING -> ResponseResult.ERROR
+    State.FINISHING -> ResponseResult.SUCCESS
+    State.NONE -> null
+}
